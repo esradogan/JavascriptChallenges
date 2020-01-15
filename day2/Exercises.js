f41f12a73e8ec8529c1959dd640b91cab0857f8a
esradogan <esradogan03@gmail.com>
2020-01-16 00:23:00 +0300

    #2 day level 3

--- day2/Exercises.js
+++ day2/Exercises.js
@@ -188,3 +188,30 @@ console.log(string8)
 //'You cannot end a sentence with because because because is a conjunction'
 
 console.log(string2.substr(31,23))
+
+
+// Exercises: Level 3
+// 'Love is the best thing in this world. Some found their love and some are still looking for their love.' 
+//Count the number of word love in this sentence.
+
+let string9 = 'Love is the best thing in this world. Some found their love and some are still looking for their love.' ;
+console.log(string9.split('love').length)
+
+
+// Use match() to count the number all because's in the following sentence:
+//'You cannot end a sentence with because because because is a conjunction'
+
+let string10 = 'You cannot end a sentence with because because because is a conjunction';
+console.log(string10.match(/because/gi).length)
+
+// Clean the following text and find the most frequent word(hint, use replace and regular express).
+//const sentence = '%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;.
+// The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;
+//I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs.
+// %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is 
+//al@so $the $resu@lt of &love& of tea&ching'
+
+const sentence ='%I $am@% a %tea@cher%, &and& I lo%#ve %te@a@ching%;. The@re $is no@th@ing; &as& mo@re rewarding as educa@ting &and& @emp%o@weri@ng peo@ple. ;I found tea@ching m%o@re interesting tha@n any ot#her %jo@bs. %Do@es thi%s mo@tiv#ate yo@u to be a tea@cher!? %Th#is 30#Days&OfJavaScript &is al@so $the $resu@lt of &love& of tea&ching';
+let arraySen = console.log(sentence.split(/\W+/gi ).join(' '))
+
+// Calculate the total annual income of the person by extract the numbers from the following text. 'He earns 5000 euro from salary per month, 10000 euro annual bonus, 15000 euro online courses per month.'
